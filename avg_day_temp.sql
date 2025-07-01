7f483cae83cfa6870a648b65e2e6d23a06eee9ef
RohrbachMatthew <matthewrohrbach1091@gmail.com>
2025-07-01 06:12:00 -0400

    Added humidity to .sql query file

--- avg_day_temp.sql
+++ avg_day_temp.sql
@@ -1,3 +1,5 @@
+/*
+--This is for the AVG daily temperature 
 -- Gets AVG temperature and makes a temp table
 with daily_average AS (
 SELECT day, date, avg(temperature) AS avg_daily_temp
@@ -8,5 +10,21 @@ GROUP BY day, date)
 Select day, count(*) as num_days,
 round(AVG(avg_daily_temp),2) AS avg_temp
 from daily_average
+group by day
+order by field(day, 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+*/
+
+
+-- This is for the avg daily humidity
+-- Get average humidity and make a temp table
+with humidity_avg as(
+select day, date, avg(humidity) as avg_daily_humidity
+from vacation_weather_data
+group by day, date)
+
+-- Average by the day
+select day, count(*) as num_days,
+round(AVG(avg_daily_humidity),2) as avg_daily_humidity
+from humidity_avg
 group by day
 order by field(day, 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
