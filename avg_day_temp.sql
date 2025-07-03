d1ace2035f32dfb1a08c9264483344ffd322c5d9
RohrbachMatthew <matthewrohrbach1091@gmail.com>
2025-07-02 21:40:42 -0400

    added humidty and temp combined in mysql

--- avg_day_temp.sql
+++ avg_day_temp.sql
@@ -30,3 +30,42 @@ from humidity_avg
 group by day
 order by field(day, 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
 */
+
+
+/*
+-- This is the average daily weather conditions (clear,cloudy,rain)
+
+with ranked_conditions as (
+SELECT day, conditions, 
+count(*) as frequency,
+rank() over (partition by day ORDER BY COUNT(*) DESC, conditions) as rn
+from vacation_weather_data
+group by day, conditions
+)
+select day, conditions
+from ranked_conditions
+where rn=1;
+*/
+
+
+-- Join table ot compare temps and humidity
+with temp AS (
+SELECT day, date, AVG(temperature) as avg_temp
+FROM vacation_weather_data
+GROUP BY day, date),
+humidity AS (
+SELECT day, date, AVG(humidity) as avg_humidity
+FROM vacation_weather_data
+GROUP BY day, date),
+merged as (
+SELECT t.day, t.date, t.avg_temp, h.avg_humidity
+from temp t
+JOIN humidity h ON t.day = h.day AND t.date = h.date
+)
+SELECT day,
+round(avg(avg_temp), 2) as avg_daily_temp,
+round(avg(avg_humidity), 2) as avg_daily_humidity
+FROM merged
+GROUP BY day
+ORDER BY FIELD(day, 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+
